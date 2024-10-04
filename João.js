02905acd964d83bfda79be9471bae88b961edc70
jpsilv9 <133126838+jpsilv9@users.noreply.github.com>
2024-10-04 11:36:40 -0300

    .,l.,

--- João.js
+++ João.js
@@ -1,2 +1,21 @@
-let nomeUsuario= 'João e Guilherme';
-alert(nomeUsuario);
+function soma(num1, num2) {
+    return num1+num2;
+   }
+   console.log(soma(6,4));
+   
+   function areaQuadrado(lado {
+       return lado**2;
+   }
+   console.log (areaQuadrado(6));
+   let media = function (num1, num2, num3){
+       return (num1+num2+num3)/3;
+   }
+   console.log (media(1,2,3));
+   let num1 = parseInt (prompt('Qual n?'));
+   let num2 = parseInt (prompt('Qual n?'));
+   let num3 = parseInt (prompt('Qual n?'));
+   let media1 = function (x,y,z){
+       return (x+y+z)/3;
+   }
+   document.write(media1(num1, num2, num3));
+   
